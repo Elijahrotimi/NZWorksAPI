8eb79558862235bf679e5c83944145dc9ca34646
Elijahrotimi <elijahrotimi@gmail.com>
2023-02-01 20:41:02 +0100

    Created Region repository interface and class

---
+++ NZWorksAPI/NZWorksAPI/Repositories/IRegionRepository.cs
@@ -0,0 +1,9 @@
+﻿using NZWorksAPI.Models.Domain;
+
+namespace NZWorksAPI.Repositories
+{
+    public interface IRegionRepository
+    {
+        IEnumerable<Region> GetAll();
+    }
+}
---
+++ NZWorksAPI/NZWorksAPI/Repositories/RegionRepository.cs
@@ -0,0 +1,19 @@
+﻿using NZWorksAPI.Data;
+using NZWorksAPI.Models.Domain;
+
+namespace NZWorksAPI.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public RegionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public IEnumerable<Region> GetAll()
+        {
+            return nZWalksDbContext.Regions.ToList();
+        }
+    }
+}
