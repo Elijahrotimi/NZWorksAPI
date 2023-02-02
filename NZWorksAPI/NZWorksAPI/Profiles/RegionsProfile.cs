7eadf09312446039441718b7e91cdc70cdabc475
Elijahrotimi <elijahrotimi@gmail.com>
2023-02-02 21:26:28 +0100

    Created automapper profile

---
+++ NZWorksAPI/NZWorksAPI/Profiles/RegionsProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace NZWorksAPI.Profiles
+{
+    public class RegionsProfile : Profile
+    {
+        public RegionsProfile()
+        {
+            CreateMap<Models.Domain.Region, Models.DTO.Region>()
+                .ReverseMap();
+        }
+    }
+}
