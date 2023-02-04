89f27c7438f7e47785b55d5b955b1b2757d25c7a
Elijahrotimi <elijahrotimi@gmail.com>
2023-02-04 16:33:42 +0100

    Created walks automapper profile

---
+++ NZWorksAPI/NZWorksAPI/Controllers/WalksController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWorksAPI.Repositories;
+
+namespace NZWorksAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalksController : Controller
+    {
+        private readonly IWalkRepository walkRepository;
+        private readonly IMapper mapper;
+
+        public WalksController(IWalkRepository walkRepository, IMapper mapper)
+        {
+            this.walkRepository = walkRepository;
+            this.mapper = mapper;
+        }
+        public async Task<IActionResult> GetAllWalksAsync()
+        {
+            var walks = await walkRepository.GetAllAsync();
+
+            var walksDTO = mapper.Map<List<Models.DTO.Walk>>(walks);
+
+            return Ok(walksDTO);
+        }
+    }
+}
