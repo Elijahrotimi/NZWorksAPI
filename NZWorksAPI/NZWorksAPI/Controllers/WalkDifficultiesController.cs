d580448dccc3d38232edaf46e6eeed269c1ac774
Elijahrotimi <elijahrotimi@gmail.com>
2023-02-06 18:52:22 +0100

    Created enpoint to get all walkdifficulties from db

---
+++ NZWorksAPI/NZWorksAPI/Controllers/WalkDifficultiesController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWorksAPI.Repositories;
+
+namespace NZWorksAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalkDifficultiesController : Controller
+    {
+        private readonly IWalkDifficultyRepository walkDifficultyRepository;
+        private readonly IMapper mapper;
+
+        public WalkDifficultiesController(IWalkDifficultyRepository walkDifficultyRepository, IMapper mapper)
+        {
+            this.walkDifficultyRepository = walkDifficultyRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalkDifficultiesAsync()
+        {
+            var walkdifficulties = await walkDifficultyRepository.GetAllAsync();
+
+            var walkdifficultiesDTO = mapper.Map<List<Models.DTO.WalkDifficulty>>(walkdifficulties);
+
+            return Ok(walkdifficultiesDTO);
+        }
+
+    }
+}
---
+++ NZWorksAPI/NZWorksAPI/Profiles/WalkDifficultiesProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace NZWorksAPI.Profiles
+{
+    public class WalkDifficultiesProfile : Profile
+    {
+        public WalkDifficultiesProfile()
+        {
+            CreateMap<Models.Domain.WalkDifficulty, Models.DTO.WalkDifficulty>()
+                    .ReverseMap();
+        }
+    }
+}
