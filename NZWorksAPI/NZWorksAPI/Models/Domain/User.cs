bdd7df0ffc413f69bde3ca263a20967a3252e937
Elijahrotimi <elijahrotimi@gmail.com>
2023-02-09 21:41:32 +0100

    Modified User model and created Roles & User_Role model

--- NZWorksAPI/NZWorksAPI/Models/Domain/User.cs
+++ NZWorksAPI/NZWorksAPI/Models/Domain/User.cs
@@ -6,8 +6,10 @@
         public string Username { get; set; }
         public string EmailAddress { get; set; }
         public string Password { get; set; }
-        public List<string> Roles { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        //Navigation property
+        public List<User_Role> UserRoles { get; set; }
     }
 }
