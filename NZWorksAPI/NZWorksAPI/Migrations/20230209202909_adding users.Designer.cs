960641fa06e83c61112ce0477853c6ccd05fd159
Elijahrotimi <elijahrotimi@gmail.com>
2023-02-09 21:42:54 +0100

    Migration to create tables in DB

--- NZWorksAPI/NZWorksAPI/Migrations/20230209202909_adding users.Designer.cs
+++ NZWorksAPI/NZWorksAPI/Migrations/20230209202909_adding users.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NZWorksAPI.Data;
 
@@ -11,9 +12,11 @@ using NZWorksAPI.Data;
 namespace NZWorksAPI.Migrations
 {
     [DbContext(typeof(NZWalksDbContext))]
-    partial class NZWalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230209202909_adding users")]
+    partial class addingusers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
